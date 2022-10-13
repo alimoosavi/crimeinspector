16d97e3fa9fc39d8d45f4eea7b833d8d1fc90930
Ali <ali.moosavi@balad.ir>
2022-10-13 18:02:21 +0330

    Add authentication to streamlit dashboard

--- streamlit_dashboard/streamlit_dashboard.py
+++ streamlit_dashboard/streamlit_dashboard.py
@@ -6,6 +6,7 @@ import streamlit as st
 from crime_primary_types import PRIMARY_TYPES
 
 CRIME_INSPECTOR_BASE_URL = os.getenv('CRIME_INSPECTOR_BASE_URL')
+AUTHENTICATION_STATIC_TOKEN = os.getenv('AUTHENTICATION_STATIC_TOKEN')
 
 
 def fetch_crimes(primary_type: str,
@@ -16,7 +17,8 @@ def fetch_crimes(primary_type: str,
         response = requests.get(f'{CRIME_INSPECTOR_BASE_URL}/crime/list',
                                 params={'primary_type': primary_type,
                                         'min_date': min_date,
-                                        'max_date': max_date})
+                                        'max_date': max_date},
+                                headers={'Token': AUTHENTICATION_STATIC_TOKEN})
         if not response.ok:
             raise Exception()
 
@@ -28,7 +30,9 @@ def fetch_crimes(primary_type: str,
 
 
 primary_type = st.sidebar.selectbox('Crime Primary Type', PRIMARY_TYPES)
-min_date = st.sidebar.date_input('Crime Min Date')
+min_date = st.sidebar.date_input('Crime Min Date',
+                                 min_value=datetime.date(year=2000, month=1, day=1),
+                                 value=datetime.date(year=2017, month=1, day=1))
 max_date = st.sidebar.date_input('Crime Max Date')
 
 map_data = pd.DataFrame(fetch_crimes(primary_type=primary_type,
